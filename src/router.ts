40f770328fed3aab0184e62c6f62cd74529ba095
LuisACG7 <cruz.guerrero.luis1jvesp@gmail.com>
2024-12-04 16:27:26 -0600

    Middleware

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,7 @@
 import { Router} from 'express'
+import { body } from 'express-validator'
 import { createProduct } from './handlers/product'
+import { handleInputErrors } from './middleware'
 
 const router = Router()
 // Routing
@@ -7,7 +9,16 @@ router.get('/', (req, res) => {
     res.json('Desde GET')
 })
 
-router.post('/', createProduct)
+router.post('/', 
+        // Validación 
+        body('name')
+        .notEmpty().withMessage('El nombre del Producto no puede ir vacío'),
+      body('price')
+        .isNumeric().withMessage('Valor no válido')
+        .notEmpty().withMessage('El precio del Producto no puede ir vacío')
+        .custom(value => value > 0).withMessage('Precio no válido'),
+    createProduct
+)
 
 router.put('/', (req, res) => {
     res.json('Desde PUT')
