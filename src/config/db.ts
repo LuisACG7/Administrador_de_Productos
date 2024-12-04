2a63e05fdba40ba9ad01d2aa885dc908757e5596
LuisACG7 <cruz.guerrero.luis1jvesp@gmail.com>
2024-12-03 19:31:52 -0600

    Conexion a la base de datos

---
+++ src/config/db.ts
@@ -0,0 +1,6 @@
+import { Sequelize } from "sequelize"
+
+const db = new Sequelize('postgresql://rest_api_node_typescript_kg2n_user:tK3S3ILN29CDA7qZSnjfJ5dntckeyqXx@dpg-ct7qfut2ng1s73esskkg-a.oregon-postgres.render.com/rest_api_node_typescript_kg2n?ssl=true')
+
+
+export default db
