ba4e4deac134afd2e6f5fdec5b6c175278474030
LuisACG7 <cruz.guerrero.luis1jvesp@gmail.com>
2024-12-03 20:08:31 -0600

    Dependencia env

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize"
+import dotenv from 'dotenv'
+dotenv.config()
 
-const db = new Sequelize('postgresql://rest_api_node_typescript_kg2n_user:tK3S3ILN29CDA7qZSnjfJ5dntckeyqXx@dpg-ct7qfut2ng1s73esskkg-a.oregon-postgres.render.com/rest_api_node_typescript_kg2n?ssl=true')
+const db = new Sequelize(process.env.DATABASE_URL!)
 
 
 export default db
