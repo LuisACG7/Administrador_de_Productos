ecb842a34f9539251117ec09451ef002764a7cd3
LuisACG7 <cruz.guerrero.luis1jvesp@gmail.com>
2024-12-05 12:15:18 -0600

    Metricas de Validacion

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -2,45 +2,30 @@ import { Request, Response} from 'express'
 import Product from '../models/Product.model'
 
 export const getProducts = async (req: Request, res: Response) => {
-    try {
-        const products = await Product.findAll({
-            order: [
-                ['price', 'DESC']
-            ],
-
-            attributes: {exclude: ['createdAt', 'updatedAt', 'availability']}
-        })
-        res.json({data: products})
-    } catch (error) {
-        console.log(error)
-    }
+  const products = await Product.findAll({
+    order: [
+        ['price', 'DESC']
+    ]
+  })
+  res.json({data: products})
 }
 
 export const getProductById = async (req: Request, res: Response): Promise<void> => {
-    try {
-      const { id } = req.params;
-      // Consulta el producto por su llave primaria
-      const product = await Product.findByPk(id);
-  
-      if (!product) {
-        res.status(404).json({ error: 'Producto No Encontrado' });
-        return;
-      }
-  
-      res.json({ data: product });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error interno del servidor' });
+   const { id } = req.params;
+   // Consulta el producto por su llave primaria
+   const product = await Product.findByPk(id);
+   
+   if (!product) {
+     res.status(404).json({ error: 'Producto No Encontrado' });
+     return;
     }
-}  
+    res.json({ data: product });
+}
+
 
 export const createProduct = async (req : Request, res: Response) => {
-    try {
-        const product = await Product.create(req.body)
-        res.status(201).json({data: product})
-    } catch (error) {
-        console.log(error)
-    }
+  const product = await Product.create(req.body)
+  res.status(201).json({data: product})
 }
 
 export const updateProduct = async (req: Request, res: Response) => {
