39e527cf8e11313c35c21f2e4c7758f27ada1801
LuisACG7 <cruz.guerrero.luis1jvesp@gmail.com>
2024-12-05 13:14:35 -0600

    Forzar errores para probar los catchs

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
-import server from '../server'
+import server, {  connectDB } from '../server'
+import db from '../config/db'
 
 
 describe('GET / api', () => {
@@ -12,5 +13,21 @@ describe('GET / api', () => {
 
         expect(res.status).not.toBe(404)
         expect(res.body.message).not.toBe('Desde api')
+    })
+})
+
+jest.mock('../config/db')
+
+describe('connectBD',() => {
+    it('Should handle database connection error', async () => {
+        jest.spyOn(db, 'authenticate')
+        .mockRejectedValueOnce(new Error('Hubo un error al conectar a la BD'))
+        const consoleSpy = jest.spyOn(console, 'log')
+
+        await connectDB()
+
+        expect(consoleSpy).toHaveBeenCalledWith(
+            expect.stringContaining('Hubo un error al conectar a la BD')
+        )
     })
 })
