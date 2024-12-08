a3e5645b9d541efb310e81490b656f3cb0fbcb58
LuisACG7 <cruz.guerrero.luis1jvesp@gmail.com>
2024-12-08 00:19:35 -0600

    Integración de la carpeta server al repositorio principal

---
+++ server/src/server.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+import colors from 'colors'
+import swaggerUI from 'swagger-ui-express'
+import swaggerSpec, { swaggerUiOptions } from './config/swagger'
+import router from './router'
+import db from './config/db'
+
+// Conectar a la base de datos
+export async function connectDB() {
+    try {
+        await db.authenticate()
+        db.sync()
+        //console.log(colors.blue.bold( 'Conectado a la BD'))
+    }catch(error) {
+        //console.log(error)
+        console.log(colors.red.bold( 'Hubo un error al conectar a la BD'))
+    }
+}
+connectDB()
+
+// Instancia de express
+const server = express()
+
+// Leer datos de formularios
+server.use(express.json())
+
+server.use('/api/products', router)
+
+// Docs
+server.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec, swaggerUiOptions) )
+
+export default server
