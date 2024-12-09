1ce5fa2c596376e0f4a7baf8a62ec422e40df3c5
LuisACG7 <cruz.guerrero.luis1jvesp@gmail.com>
2024-12-08 18:23:22 -0600

    Funcionalidad

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,7 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Layout from './Layouts/Layout'
-import Products from './views/Products'
+import Products, { loader as productsLoader } from './views/Products'
 import NewProduct, { action as newProductAction } from './views/NewProduct'
+import EditProduct, { loader as editProductLoader, action as editProductAction} from './views/EditProduct'
+import { action as deleteProductAction } from './components/ProductDetails'
+
 
 export const router = createBrowserRouter([
     {
@@ -10,13 +13,24 @@ export const router = createBrowserRouter([
         children: [
             {
                 index: true,
-                element: <Products />
+                element: <Products />,
+                loader: productsLoader,
             },
             {
                 path: 'productos/nuevo',
                 element: <NewProduct />,
                 action: newProductAction
+            },
+            {
+                path: 'productos/:id/editar', //ROA Pattern -Resource-oriented design
+                element: <EditProduct />, 
+                loader: editProductLoader,
+                action: editProductAction
+            },
+            {
+                path: 'productos/:id/eliminar',
+                action: deleteProductAction
             }
-        ]
+        ],
     }
 ])
--- client/src/services/ProductService.ts
+++ client/src/services/ProductService.ts
@@ -1,6 +1,7 @@
-import { safeParse } from 'valibot'
+import { safeParse, number, parse, coerce} from 'valibot'
 import axios from 'axios'
-import { DraftProductSchema } from "../types"
+import { DraftProductSchema, ProductsSchema, Product, ProductSchema } from "../types"
+import { toBoolean } from '../utils';
 
 
 type ProductData = {
@@ -24,5 +25,64 @@ export async function addProduct(data: ProductData) {
         }
     } catch (error) {
         console.error(error)
+    }
+}
+
+export async function getProducts() {
+    try {
+        const url = `${import.meta.env.VITE_API_URL}/api/products`
+        const { data } = await axios(url)
+        const result = safeParse(ProductsSchema, data.data)
+        if(result.success) {
+            return result.output
+        } else {
+            throw new Error('Hubo un error...')
+        }
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export async function getProductById(id: Product['id']) {
+    try {
+        const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`
+        const { data } = await axios(url)
+        const result = safeParse(ProductSchema, data.data)
+        if(result.success) {
+            return result.output
+        } else {
+            throw new Error('Hubo un error...')
+        }
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export async function updateProduct(data : ProductData, id : Product['id']) {
+    try {
+
+        const NumberSchema = coerce(number(), Number)
+
+        const result = safeParse(ProductSchema, {
+            id,
+            name: data.name,
+            price: parse(NumberSchema, data.price),
+            availability: toBoolean(data.availability.toString())
+        })
+        if(result.success) {
+            const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`
+            await axios.put(url, result.output)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function deleteProduct(id: Product['id']) {
+    try {
+        const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`
+        await axios.delete(url)
+    } catch (error) {
+        console.log(error)
     }
 }
